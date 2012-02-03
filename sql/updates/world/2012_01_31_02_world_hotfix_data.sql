ca1fa4999d0a1a1352374b1bc3cb396e8294cc96
SumDumGuy <sumdumguy@kpsh.us>
2012-02-03 16:14:55 -0500

    Update 2012_01_31_02_world_hotfix_data.sql with a a proper time syntax

--- sql/updates/world/2012_01_31_02_world_hotfix_data.sql
+++ sql/updates/world/2012_01_31_02_world_hotfix_data.sql
@@ -1,4 +1,4 @@
-ALTER TABLE hotfix_data ADD `hotfixDate` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `unk`;
+ALTER TABLE hotfix_data ADD `hotfixDate` DATETIME NOT NULL DEFAULT '1970-01-01 00:00:01' AFTER `unk`;
 UPDATE `hotfix_data` SET `hotfixDate`=FROM_UNIXTIME(`unk`);
 ALTER TABLE `hotfix_data` DROP PRIMARY KEY;
 ALTER TABLE `hotfix_data` ADD PRIMARY KEY (`entry`,`type`,`hotfixDate`);
